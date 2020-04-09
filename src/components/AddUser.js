c1b706dea374de6e1cf9c2099aeecdcbea7480c3
Lucas Dramowicz <ldramowicz@yahoo.com>
2020-04-09 16:01:32 -0400

    alert added

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
 
 const AddUser = ({addUser}) => {
     const defaultFormInfo = { id: null, firstName: '', lastName: '' };
     const [user, setUser] = useState(defaultFormInfo);
+    const [error, setError] = useState(false);
 
     const onInputChange = event => {
         const {name, value} = event.target;
@@ -12,24 +14,28 @@ const AddUser = ({addUser}) => {
     };
 
     return (
-        <form
-            onSubmit={event => {
-                event.preventDefault();
-                if (!user.firstName || !user.lastName) {
-
-                    return;
-                }
-                console.log("asdasdasd")
-                addUser(user)
-                setUser(defaultFormInfo)
-            }}
-        >
-            <label>First Name</label>{' '}
-            <input type="text" name="firstName" value={user.firstName} onChange={onInputChange} />{' '}
-            <label>Last Name</label>{' '}
-            <input type="text" name="lastName" value={user.lastName} onChange={onInputChange} />{' '}
-            <Button type="submit" variant="outline-primary">Add User</Button>
-        </form>
+        <div>
+            {error && <Alert variant="warning">asd</Alert>}
+            <form
+                onSubmit={event => {
+                    event.preventDefault();
+                    if (!user.firstName || !user.lastName) {
+                        setError(true);
+                       return;
+                    }
+                    console.log("asdasdasd");
+                    addUser(user);
+                    setUser(defaultFormInfo)
+                    setError(false);
+                }}
+            >
+                <label>First Name</label>{' '}
+                <input type="text" name="firstName" value={user.firstName} onChange={onInputChange} />{' '}
+                <label>Last Name</label>{' '}
+                <input type="text" name="lastName" value={user.lastName} onChange={onInputChange} />{' '}
+                <Button type="submit" variant="outline-primary">Add User</Button>
+            </form>
+        </div>
     )
 };
 
